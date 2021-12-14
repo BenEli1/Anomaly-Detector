c059fe3981bf35b885f970f87df6273b5ad3c58d
BenELi1 <ben.eli@live.biu.ac.il>
2021-12-14 13:13:42 +0200

    Changes by sahar to Ex4 final submit

--- HybridAnomalyDetector.cpp
+++ HybridAnomalyDetector.cpp
@@ -17,20 +17,30 @@ float HybridAnomalyDetector::distanceCalc(Point p,correlatedFeatures correlatedF
         SimpleAnomalyDetector::distanceCalc(p,correlatedFeature);
     }
 }
+
+//the function create hybrid correlatedFeatures depending on the correlation between the features
 void HybridAnomalyDetector::sortCorrelatedFeatures(float maxCor, const TimeSeries& ts, int i, int c){
+    //if the correlation > 0.9, create correlatedFeatures by calculate the line
     if (maxCor >= getCorrelationThreshold()) {
         SimpleAnomalyDetector::sortCorrelatedFeatures(maxCor,ts,i,c);
     }
+    //if the correlation > 0.5, create correlatedFeatures by calculate the minimum circle
     else if (maxCor >= getCorrelationCircleThreshold()){
+        //features list
         vector<string> featureList(ts.getFeatures());
+        //the number of rows
         int numOfValues = ts.getSizeOfValues();
+        //the measurments in feature i and feature c
         float *x = ts.getMeasurements(featureList[i]);
         float *y = ts.getMeasurements(featureList[c]);
+        //create array of Point* from the measurments above
         Point* points[numOfValues];
         for (int i = 0; i < numOfValues; i++){
             points[i] = new Point(x[i], y[i]);
         }
+        //create the smallest circle from the points of features x and y
         Circle enclosingCircle = findMinCircle(points,numOfValues);
+        //create correlatedFeatures of x and y
         correlatedFeatures associated{
             .isHybrid = true ,.feature1 = featureList[i],
                 .feature2 = featureList[c],
@@ -38,7 +48,9 @@ void HybridAnomalyDetector::sortCorrelatedFeatures(float maxCor, const TimeSerie
                 .lin_reg = Line(0,0),
                 .threshold = enclosingCircle.radius*getThresholdMultiplier(),
                 .minCircle = enclosingCircle};
+        //add correlatedFeatures to the list
         cf.push_back(associated);
+        //free memory allocations
         for (int i = 0; i < numOfValues; i++){
             delete points[i];
         }
