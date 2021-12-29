e256a1b61af9b5c7e9d1ef23cf3d38b296877ec4
Ben Eli <benelashvili@gmail.com>
2021-12-29 10:50:26 -0800

    added the menu printing

--- CLI.h
+++ CLI.h
@@ -13,8 +13,23 @@ class CLI {
     map<int, Command> commandMap;
 	// you can add data members
 public:
-	CLI(DefaultIO* dio);
-	void start();
+	CLI(DefaultIO* dio):dio(dio){
+        commandMap = map<int, Command>();
+        commandMap.insert(pair<int, Command>(1,uploadFileCommand(dio)));
+        commandMap.insert(pair<int, Command>(2,algoSettingsCommand(dio)));
+        commandMap.insert(pair<int, Command>(3,detectAnomaliesCommand(dio)));
+        commandMap.insert(pair<int, Command>(4,displayResultsCommand(dio)));
+        commandMap.insert(pair<int, Command>(5,uploadAndAnalyzeCommand(dio)));
+        commandMap.insert(pair<int, Command>(6,exitCommand(dio)));
+    };
+	void start(){
+        int size = commandMap.size();
+        dio->write("Welcome to the Anomaly Detection Server.\n");
+        dio->write("Please choose an option:\n");
+        for(int i = 1 ; i<=size ; i++){
+            dio->write(commandMap[i].getDescription());
+        }
+    }
 	virtual ~CLI();
 };
 
